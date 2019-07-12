10cfeb5ec0deee9a53336747da8f3d5ac05e98a9
Sape123 <samulipelt@gmail.com>
2019-07-12 12:23:24 +0300

    osa 3 ensimmäisen osan tehtävät

---
+++ index.js
@@ -0,0 +1,183 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+var morgan = require('morgan')
+app.use(morgan('tiny'))
+
+console.log(morgan(':method :url :status :res[content-length] - :response-time ms'))
+
+let numerot = [  { 
+       id: 1,   
+        name: "Arto Hellas",  
+         number: "040-12312414" 
+             },
+    {  
+          id: 2, 
+             name: "Ada Lovelace",  
+             number: "39445325252"
+      },
+        {   
+             id: 3, 
+                name: "Dan Abramov ", 
+                number: "392452525252" 
+     },
+    {
+        id: 4,
+        name:"Mary Poppendieck",
+        number:"39225525525"
+    }]
+
+
+
+
+app.get('/info', (req, res) => {
+
+
+
+    let aika = new Date().toLocaleString();
+
+const sivu = () => {
+  
+return (
+    `Phonebook has info for ${numerot.length} people  <br> ${aika}` 
+
+)
+
+}
+
+
+  res.send(sivu())
+
+})
+
+
+app.delete('/api/persons/:id', function (req, res) {
+  const id = Number(req.params.id)
+  numerot = numerot.filter(numero => numero.id !== id)
+  res.status(404).end()
+
+
+})
+
+
+
+
+
+
+app.post('/api/persons', (request, response) => {
+  
+
+
+  const body = request.body
+console.log(body)  
+
+
+let rId = Math.random(1000000)
+
+if(!body.name){
+
+
+  console.log(request.headers)
+  return response.status(400).json({
+  
+  error: 'no content'
+  
+  })
+  
+  }
+
+nimet = numerot.map(nam => nam.name)
+
+
+
+ 
+
+  var found = nimet.find(function(element) {
+    return element === body.name;
+  });
+
+
+
+
+if( typeof found === 'string'){
+  return response.status(400).json({
+  
+    error: 'nimi on jo'
+    
+    })
+
+}
+
+
+
+
+
+
+
+
+ 
+  
+const numero = {
+  id: rId,
+name: body.name,
+number: body.number
+
+
+}
+
+
+numerot = numerot.concat(numero)
+  
+
+
+  response.json(numero)
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+app.get('/api/persons', (req, res) => {
+
+
+
+  res.json(numerot)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+
+
+    const id = Number(request.params.id)
+    const numero = numerot.find(numerot => numerot.id === id)
+    
+
+if(numero){
+    response.json(numero)
+
+
+}
+else{
+   
+
+
+    response.status(404).end()
+}  
+
+})
+
+
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
