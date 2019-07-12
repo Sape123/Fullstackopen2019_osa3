15e8c2119b7dd28767ebc7313a4ad26eea710fdc
Sape123 <samulipelt@gmail.com>
2019-07-12 23:02:22 +0300

    fortnedn build

---
+++ build/precache-manifest.abee9a3cd2300e2675dc2b9e1c8303bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9759b331ab44f352d4cd7a70ab7257a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13149c608fad3d3f08d7",
+    "url": "/static/css/main.d6d8c5ba.chunk.css"
+  },
+  {
+    "revision": "6a19a0beea039a280f26",
+    "url": "/static/js/2.3c62b441.chunk.js"
+  },
+  {
+    "revision": "13149c608fad3d3f08d7",
+    "url": "/static/js/main.ce0db932.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
